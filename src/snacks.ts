c6309ffe61d61e84352d509ff6f6808ac2692598
sabella <ssaulino@ucsd.edu>
2025-10-16 11:36:24 -0700

    try

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,8 +1,15 @@
 // src/snacks.ts
 const snacks: string[] = ['yogurt', 'chips', 'fruit', 'cookies', 'corndogs', 'soda'];
 
-export function print() {
-  for (const snack of snacks) {
+export function printSnacks() {
+    for (const snack of snacks) {
+        animateFeature(snack, 'italic');
+    }
+}
+//little comment 
+
+const snacks1: string[] = ['cookies', 'fruit', 'corndogs','chips','soda']
+for (const snack of snacks) {
     console.log(snack);
   }
 }
